27de6e7bf7f127e2b26cb05503c1e812337180aa
Donni Richasdy <donni.richasdy@gmail.com>
2016-06-03 17:49:33 +0700

    back to normal

--- src/main/java/com/richtech/flightradar24/Application.java
+++ src/main/java/com/richtech/flightradar24/Application.java
@@ -1,17 +1,17 @@
+package com.richtech.flightradar24;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.richtech.flightradar24;
 
 /**
  *
  * @author richasdy
  */
-public class App {
-    
+public class Application {
     public static void main (String[] args){
-        System.out.println("hello world");
+        System.out.println("hello?");
     }
 }
